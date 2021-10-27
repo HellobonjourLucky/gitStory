db3cf7723b221d22a068457c490c03da72c99433
HellobonjourLucky <73243458+HellobonjourLucky@users.noreply.github.com>
2021-10-27 20:13:23 +0200

    delete comment

--- todoList_server/app.js
+++ todoList_server/app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-//mongoose connection
+
 mongoose.connect('mongodb://localhost:27017/todolistDB');
 
 const itemSchema = {
