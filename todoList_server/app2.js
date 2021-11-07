1e58ba2d7efa92f906c33803bb62d47c33575dbb
HellobonjourLucky <73243458+HellobonjourLucky@users.noreply.github.com>
2021-11-07 19:30:04 +0100

    make get/post method

--- todoList_server/app2.js
+++ todoList_server/app2.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+const items = ["book", "cleaning"];
+
+
+app.get('/', function(req, res){
+  res.render('list2', {listTitle : "Today", items: items});
+});
+
+app.post('/', function(req,res){
+  const newItem = req.body.newItem;
+  items.push(newItem);
+});
 
 app.listen(3000,function(){
   console.log("app2: 200");
-})
+});
