43204a91b7c883df4c99ab31e53af937e6c1790d
HellobonjourLucky <73243458+HellobonjourLucky@users.noreply.github.com>
2021-11-10 13:38:13 +0100

    add mongoose insert()

--- todoList_server/app2.js
+++ todoList_server/app2.js
@@ -8,161 +8,94 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 
-mongoose.connect('mongodb://localhost:27017/fruitsDB');
+mongoose.connect('mongodb://localhost:27017/tododb');
 
-let items = ["book", "cleaning"];
-let workItems = [];
-
-const fruitsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    reqired: [1, "Please check your data entry, no name specified!"]
-  },
-  rating: {
-      type: Number,
-      min: 1,
-      max: 10
-  },
-  review: String
+const todoSchema = new mongoose.Schema({
+  name: String
 });
 
-const personSchema = new mongoose.Schema({
+const listSchema = new mongoose.Schema({
   name: String,
-  age: Number,
-  favoriteFruit: fruitsSchema
-})
-
-const FruitModel = mongoose.model('Fruit', fruitsSchema);
-const PersonModel = mongoose.model('Person', personSchema);
-
-const apple = new FruitModel({
-  name: 'Apple',
-  rating: 5,
-  review: 'nice'
+  items: [todoSchema]
 });
 
-const kiwi = new  FruitModel({
-  name: "Kiwi",
-  rating: 10,
-  review: "The best fruit"
+const TodoModel = mongoose.model('Todo', todoSchema);
+const ListModel = mongoose.model('List', listSchema);
+
+const item1 = new TodoModel({
+  name: "Welcome to your todoList"
+});
+const item2 = new TodoModel({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new TodoModel({
+  name: "<--Hit this to delete the item"
 });
 
-const orange = new  FruitModel({
-  name: "Orange",
-  rating: 4,
-  review: "Too sour for me"
-});
+const defaultItems = [item1, item2, item3];
 
-const banana = new  FruitModel({
-  name: "Banana",
-  rating: 3,
-  review: "Weird texture"
-});
 
-const john = new PersonModel({
-  name: 'John',
-  age: 24
-})
-
-const ammy = new PersonModel({
-  name: "Ammy",
-  age: 31,
-  favoriteFruit: apple
-})
-
-// apple.save();
-// john.save();
-// ammy.save();
-
-// FruitModel.insertMany([kiwi, banana, apple], function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log('successfully saved all the fruits to fruitsDB');
-//   }
-// })
-
-// FruitModel.updateOne({_id:"618abc0ff17c866247375dd7"}, {name: "Peach"}, function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully updated the document");
-//   }
-// });
-//
-// PersonModel.updateOne({name:"John"}, {favoriteFruit: banana}, function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully updated the document");
-//   }
-// })
-//
-// FruitModel.deleteOne({_id: "618abc0ff17c866247375dd8"}, function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully deleted the document");
-//   }
-// })
-
-// PersonModel.deleteMany({name: "Ammy"},function(err){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log("Successfully deleted the document");
-//   }
-// });
-
-PersonModel.find(function(err, data){
-  if(err){
-    console.log(err);
-  }else{
-    console.log(data);
-  }
-});
 
 app.get('/', function(req, res){
-  res.render('list2', {listTitle : "Today", items: items});
+  TodoModel.find(function(err, data){
+    if(data.length === 0){
+      TodoModel.insertMany(defaultItems, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Successfully saved default items to DB");
+        }
+        res.redirect('/');
+      });
+    }else{
+      res.render('list2', {listTitle : "Today", items: data});
+    }
+  })
 });
 
 
 app.get('/:customListName', function(req,res){
   const customListName = req.params.customListName;
-  if(customListName === 'work'){
-    res.render('list2', {listTitle : customListName, items: workItems})
-  }else{
-    res.render('list2', {listTitle : customListName, items: items});
-  }
-})
+  ListModel.findOne({name: customListName}, function(err, data){
+    if(!err){
+      if(!data){
+        //make a new List!
+        console.log("Not exist this List! Make new One!");
+        const newList = new ListModel({
+          name: customListName,
+          items: defaultItems
+        });
+        newList.save();
+        res.redirect('/' + customListName);
+      }else{
+        //showing existing List!
+        // console.log(data);
+        res.render('list2', {listTitle: data.name , items: data.items});
+      }
+    }
+  })
+});
 
 
 app.post('/', function(req,res){
   const newItem = req.body.newItem;
-  const listName = req.body.listName;
-  if(listName === "Today"){
-    items.push(newItem);
-    res.redirect('/');
-  }else{
-    workItems.push(newItem);
-    res.redirect('/' + 'work');
-  }
+  const item = new TodoModel({
+    name: newItem
+  });
+  item.save();
+  res.redirect('/');
 });
 
 app.post('/delete', function(req,res){
   const removeItem = req.body.checkbox;
-  const listName = req.body.listName;
-  if(listName === "Today"){
-    items = items.filter(item => {
-      return item !== removeItem;
-    })
-    res.redirect('/');
-  }else{
-    workItems = workItems.filter(item => {
-      return item !== removeItem;
-    })
-    res.redirect('/' + 'work');
-  }
+  TodoModel.deleteOne({_id: removeItem}, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Successfully deleted the item from DB");
+    }
+  res.redirect('/');
+  })
 })
 
 app.listen(2000,function(){
