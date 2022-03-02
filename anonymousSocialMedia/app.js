3afe1e38e3a045061398a61c9de3a0bc7489bdc4
HellobonjourLucky <hellobonjour.lucky@gmail.com>
2022-03-02 19:06:54 +0100

    index.ejs setting

--- anonymousSocialMedia/app.js
+++ anonymousSocialMedia/app.js
@@ -2,16 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+let items = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
+    const covid = new Date(2019, 12, 31);
     const today = new Date();
-    const currentDay = today.getDay();
 
-    if(currentDay === 6 || currentDay === 0){
-        res.sendFile(__dirname + "/index.html");
-    }else{
-        res.sendFile(__dirname + "/index.html");
-    }
+    // One day in milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    const diffInTime = today.getTime() - covid.getTime();
+    const diffInDays = Math.round(diffInTime / oneDay);
+
+    res.render('index', {Dday: diffInDays, newTexts: items});
+})
+
+app.post('/', function(req, res){
+    items.push(req.body.newText);
+    res.redirect('/');
 })
 
 app.listen(3000, function(){
