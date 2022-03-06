d61b4ac41064ef3e5654375de94124ea34666610
HellobonjourLucky <hellobonjour.lucky@gmail.com>
2022-03-06 16:52:52 +0100

    complete node

--- anonymousSocialMedia/app.js
+++ anonymousSocialMedia/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const _ = require('lodash');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost27017/postsDB');
+// const _ = require('lodash');
+// const { isBuffer } = require('lodash');
 
 const app = express();
-const posts = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,6 +12,35 @@ app.use(express.static(__dirname + '/public'));
 const covid = new Date(2019, 12, 31);
 const today = new Date();
 
+
+//MongoDB connect
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/postsDB');
+
+const postSchema = {
+    content: String,
+    date: String
+ }
+
+const PostModel = mongoose.model('Post', postSchema);
+
+const post1 = {
+    content: "Welcome to KABK comfort garden.",
+    date: "Sun Mar 06 2022"
+}
+const post2 = {
+    content: "You must be depressed and anxious during this pandemic ",
+    date: "Sun Mar 06 2022"
+}
+const post3 = {
+    content: "But you are not alone...",
+    date: "Sun Mar 06 2022"
+}
+
+const defalutPosts = [post1, post2, post3];
+
+
+
 app.get('/', function(req, res){
     // One day in milliseconds
     const oneDay = 1000 * 60 * 60 * 24;
@@ -21,29 +48,44 @@ app.get('/', function(req, res){
     const diffInTime = today.getTime() - covid.getTime();
     const diffInDays = Math.round(diffInTime / oneDay);
 
-    res.render('index', {Dday: diffInDays, postCollection: posts});
+    PostModel.find({}, function(err, foundPosts){
+        if(foundPosts.length === 0){
+            PostModel.insertMany(defalutPosts, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("Successfully saved default posts to DB");
+                }
+            })
+            res.redirect('/');
+        }else{
+            res.render('index', {Dday: diffInDays, postCollection: foundPosts});
+        }
+    })
 });
 
 app.get('/posts/:postDate', function(req, res){
-    const requiredDate = _.lowerCase(req.params.postDate);
-    posts.forEach(post => {
-        const storedDate = _.lowerCase(post.id);
-        console.log("sotred date is..." + storedDate);
-        if(storedDate == requiredDate){
-            console.log('Match!');
-            return res.render('post', {post:post});
-        }
+    const requiredDate = req.params.postDate;
+
+    PostModel.find({}, function(err, foundPosts){
+        foundPosts.forEach(post => {
+            const storedDate = post._id;
+            console.log("sotred date is..." + storedDate);
+            if(storedDate == requiredDate){
+                console.log('Match!');
+                return res.render('post', {post:post});
+            }
+        })
     })
 })
 
 
 app.post('/', function(req, res){
-    const post = {
-        date : today.toDateString(),
-        content : req.body.postBody,
-        id: Math.random(0,1)
-      }
-    posts.push(post);
+    const post = new PostModel({
+        content: req.body.postBody,
+        date: today.toDateString()
+    })
+    post.save();
     res.redirect('/');
 })
 
